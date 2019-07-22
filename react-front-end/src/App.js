021860e6509e5172c67744a6545eb26651366e5f
govsandhu <gc.sandhu@gmail.com>
2019-07-22 21:34:22 +0000

    Created navbar with initial  subcomponents

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,37 +1,23 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Navbar from './components/navbar/navBar'
 import './App.css';
+import 'antd/dist/antd.css';
+
+
+
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      message: 'Click the button to load data!'
     }
   }
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
 
   render() {
-    return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
-    );
+    return <Navbar />
   }
 }
 
