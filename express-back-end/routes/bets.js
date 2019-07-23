8869eb26e6f901db7fbf2e033521889b8645057f
govsandhu <gc.sandhu@gmail.com>
2019-07-23 03:39:14 +0000

    Created backed route to add bet participant emails into the database

--- express-back-end/routes/bets.js
+++ express-back-end/routes/bets.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require("../models");
 const Bet = db.Bet;
 const UserBet = db.User_Bet;
+const User = db.User;
 
 router.post("/", (req, res) => {
   const newBet = {
@@ -19,27 +20,47 @@ router.post("/", (req, res) => {
 
       const ownerBet = {
         user_id: userid,
-        bet_id: betid
+        bet_id: betid,
+        teamSelect: req.body.team
       };
 
       return UserBet.create(ownerBet);
     })
     // create participant 
     .then(userbet => {
-      const participantIds = req.body.participantIds;
-      const participantBets = participantIds.map(participantId => {
-        return UserBet.create({
-          bet_id: userbet.bet_id,
-          user_id: participantId
-        });
-      });
-      return Promise.all(participantBets);
-    })
-    .then(rez => {
-      res.status(201).send("Bet created");
-    })
-    .catch(err => {
-      console.log("Some errors here: " + err);
+      
+      const participantEmails = req.body.emails;
+      let participantIds = [];
+
+      participantEmails.map(participantEmail => {
+       User.findOne({ where: { email: participantEmail }})
+        .then(result => {
+          // participantIds.push(result.get("id"))
+          UserBet.create({
+            bet_id: userbet.bet_id,
+            user_id: result.get("id")
+          }).then(rez => {
+            res.status(201).send("Bet created");
+          })
+          .catch(err => {
+            console.log("Some errors here: " + err);
+          });
+        })
+      })
+
+    //   const participantBets = participantIds.map(participantId => {
+    //     return UserBet.create({
+    //       bet_id: userbet.bet_id,
+    //       user_id: participantId
+    //     });
+    //   });
+    //   return Promise.all(participantBets);
+    // })
+    // .then(rez => {
+    //   res.status(201).send("Bet created");
+    // })
+    // .catch(err => {
+    //   console.log("Some errors here: " + err);
     });
 });
 
