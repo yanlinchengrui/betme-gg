3e0a74a90e6793da8d9fb658a7f53b5cfcf3f275
Nick <nbjenvey@gmail.com>
2019-07-28 04:39:51 +0000

    Background styles

--- react-front-end/src/components/dashboard/Dashboard.js
+++ react-front-end/src/components/dashboard/Dashboard.js
@@ -1,13 +1,15 @@
-import React, { Component } from "react";
-import ActiveBetsList from "./ActiveBetsList";
-import UpcomingMatches from "./UpcomingMatches";
+import React, { Component } from 'react';
+import ActiveBetsList from './ActiveBetsList';
+import UpcomingMatches from './UpcomingMatches';
+import bg from '../../images/bg.jpg'
 
 class Dashboard extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="dashboard">
+      <div className='container'>
+        <img src={bg} className='bg' />
+        <div className='dashboard'>
           <UpcomingMatches upcomingMatches={this.props.upcomingMatches} />
           <ActiveBetsList activeBets={this.props.activeBets} />
         </div>
