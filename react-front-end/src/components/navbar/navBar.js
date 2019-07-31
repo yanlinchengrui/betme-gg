4a0cc688d95f7493b7a3eb8c796af73562c126fa
yanlinchengrui <yanlinchengrui@hotmail.com>
2019-07-31 23:29:51 +0000

    fix links to live

--- react-front-end/src/components/navbar/navBar.js
+++ react-front-end/src/components/navbar/navBar.js
@@ -20,39 +20,39 @@ class NavBar extends Component {
           </div>
           <Menu mode='horizontal' selectable={false}>
             <Menu.Item>
-              <Link to='/stream/lol'>
+              <a href='http://localhost:3000/stream/lol'>
                 <img src={lol} alt='League of Legends Stream' style={{ width: '24px', height: '24px' }} />
-              </Link>
+              </a>
             </Menu.Item>
             <Menu.Item>
-              <Link to='/stream/csgo'>
+              <a href='http://localhost:3000/stream/csgo'>
                 <img src={csgo} alt='Counter Strike: Global Offensive Stream' style={{ width: '24px', height: '24px' }} />
-              </Link>
+              </a>
             </Menu.Item>
             <Menu.Item>
-              <Link to='/stream/ow'>
+              <a href='http://localhost:3000/stream/ow'>
                 <img src={ow} alt='Overwatch Stream' style={{ width: '24px', height: '24px' }} />
-              </Link>
+              </a>
             </Menu.Item>
             <Menu.Item>
-              <Link to='/stream/dota2'>
+              <a href='http://localhost:3000/stream/dota2'>
                 <img src={dota} alt='Dota 2 Stream' style={{ width: '24px', height: '24px' }} />
-              </Link>
+              </a>
             </Menu.Item>
           </Menu>
 
           <div className='notifications'>
             <NotificationList
-                userBets={this.props.userBets}
-                handleNotificationRead={this.props.handleNotificationRead}
-                handleNotificationSelection={this.props.handleNotificationSelection}
-              />
+              userBets={this.props.userBets}
+              handleNotificationRead={this.props.handleNotificationRead}
+              handleNotificationSelection={this.props.handleNotificationSelection}
+            />
           </div>
           <div className='user-options'>
-          <UserOptions userInfo={this.props.userInfo}/>
+            <UserOptions userInfo={this.props.userInfo} />
           </div>
           <div className='new-bet'>
-            <NewBet upcomingMatches={this.props.upcomingMatches} refreshComponent={this.props.refreshComponent} bank={this.props.userInfo.bank}/>
+            <NewBet upcomingMatches={this.props.upcomingMatches} refreshComponent={this.props.refreshComponent} bank={this.props.userInfo.bank} />
           </div>
 
         </div>
